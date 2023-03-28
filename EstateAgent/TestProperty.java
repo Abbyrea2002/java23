283c6b0492b60f869c8bd4b112fa3bae290428d7
Abbyrea2002 <abbyrea2002@gmail.com>
2023-03-28 17:37:28 +0100

    updatez

--- EstateAgent/TestProperty.java
+++ EstateAgent/TestProperty.java
@@ -1,15 +1,20 @@
 package EstateAgent;
 
 import EstateAgent.Property;
+import EstateAgent.ArrayOfProperties;
+
+import java.util.Properties;
 
 public class TestProperty
 {
    public static void main(String args[])
    {
+      Property oneProperty = new Property("Recreation Road ", "Larne ", "Bt40 1ew ", "semi detached", "richard green", "07565899835", 1);
+      properties.insert(oneProperty);
+
+      properties.display
 
 
-      property oneProperty = new property("Recreation Road ", "Larne ", "Bt40 1ew ", "semi detached", "richard green", "07565899835", 1);
-      oneProperty.displayProperty();
 
    }//main
 }//class
