64f80b6eaf2269dd5719fabd80c94b7c8ea176d8
ABBY REA-HUMPHRYS <60029935@nrc.ac.uk>
2023-04-18 16:51:45 +0100

    update

--- EstateAgent/PropertyManagment.java
+++ EstateAgent/PropertyManagment.java
@@ -7,7 +7,24 @@ import java.util.*;
 
 public class PropertyManagment
 {
+ protected static void passwordValidation()
+ {
+    Scanner sc = new Scanner(System.in);
+    String password = "estate";
+    String pass;
+    System.out.println("Enter password: ");
+    pass = sc.next();
+    if (!(pass.equals(password)))
+    {
+    do
+    {
+       System.out.println("Incorrect please re-enter: ");
 
+       pass = sc.nextLine();
+
+    }while(!(pass.equals(password)));}
+
+ }
    public static void main(String args[])
 
    {
@@ -19,11 +36,11 @@ public class PropertyManagment
       ArrayOfProperties1 MyArray = new ArrayOfProperties1();
 
       //creates property objects
-      Property oneProperty = new Property("Recreation Road ", "Larne ", "Bt40 1ew ", "Semi-Detached", "Richard Green", "07565899835", 100000);
-      Property twoProperty = new Property ("Lindara Drive ", "Carrick ", "BT40 2JC", "Detached", "Sandra Nelson", "07999365757", 150000);
-      Property threeProperty = new Property ("Herbet Avenue ", "Larne ", "BT40 AB5", "Semi-Detached", "Richard Green", "07654277543", 200000);
-      Property fourProperty = new Property ("Argyll Avenue ", "Portrush ", "BT5 2JX", "Detached", "Ash Wilson", "07898655432", 250000);
-      Property fiveProperty = new Property ("Sallagh park ", "larne ", "BT40 3GQ", "Terrace", "Sandra Nelson", "07789677754", 100000);
+      Property oneProperty = new Property("a", "Larne ", "Bt40 1ew ", "Semi-Detached", "Richard Green", "07565899835", 100000);
+      Property twoProperty = new Property ("LindaraDrive", "Carrick ", "BT40 2JC", "Detached", "Sandra Nelson", "07999365757", 150000);
+      Property threeProperty = new Property ("HerbetAvenue", "Larne ", "BT40 AB5", "Semi-Detached", "Richard Green", "07654277543", 200000);
+      Property fourProperty = new Property ("ArgyllAvenue", "Portrush ", "BT5 2JX", "Detached", "Ash Wilson", "07898655432", 250000);
+      Property fiveProperty = new Property ("Sallaghpark", "larne ", "BT40 3GQ", "Terrace", "Sandra Nelson", "07789677754", 100000);
 
       //inserts property details from oneProperty object into array
       MyArray.insert(oneProperty);
@@ -55,36 +72,146 @@ public class PropertyManagment
 
                 switch (option)
                 {
-                   case 0:
+                   case '0':
                       break;
-                   case 1:
-                     //displays property details when user enters 1 using contructor from ArrayOfProperties
-                     MyArray.displayAllProperties();
-                   case 2:
-                     System.out.println("What town is the property in?");
-                     String town = sc.next();
-                   case 3:
-                   case 4:
+                   case '1':
+                      //displays property details when user enters 1 using contructor from ArrayOfProperties
+                      MyArray.displayAllProperties();
+                   case '2':
+                      System.out.println("What town is the property in?");
 
-                   case 5:
-                     System.out.println("What is the password?:    ");
-                     password = sc.next();
-                     if (password == "Admin")
-                     {
-                        System.out.println("");
-                     } else
-                     {
-                        System.out.println("Invalid password - please re-enter");
-                     }
+                   case '3':
+                      String type = null;
+                      String phoneNumber = null;
+                      String price = null;
+                      int n = 0;
+                      int choice;
+                      String town1 = null;
+                      String postCode = null;
+                      String vendorName = null;
+
+                      passwordValidation();
+                      System.out.print("please enter the street: ");
+                      String street1 = sc.next();
+                 
+                      for (int i = 0; i < MyArray.getNoOfProperties(); i++)
+                      {
+                         MyArray.getStreet(i);
+                         if (street1.equals(MyArray.getStreet(i)))
+                         {
+                            System.out.print("a");
+                            System.out.println("This property already exists - please try again");
+                            street1 = sc.next();
+                         } else
+                         {
+
+
+                            System.out.println("Please enter the town: ");
+                            town1 = sc.next();
+                            System.out.println("Please enter postcode: ");
+                            postCode = sc.next();
+                            System.out.println("Enter name of vendor: ");
+                            vendorName = sc.next();
+                            do
+                            {
+                               System.out.println("Which type is the house?");
+                               System.out.println("1- Detached");
+                               System.out.println("2- Semi-Detached");
+                               System.out.println("3- Terraced");
+                               System.out.println("4- Apartment");
+                               System.out.println("Enter your choice");
+                               choice = sc.nextInt();
+                               if (choice == 1)
+                               {
+                                  type = "Detached";
+                               }
+                               if (choice == 2)
+                               {
+                                  type = "Semi-Detached";
+                               }
+                               if (choice == 3)
+                               {
+                                  type = "Terraced";
+                               }
+                               if (choice == 4)
+                               {
+                                  type = "Apartment";
+                               } else
+                               {
+                                  System.out.println("this is an invalid choice - please re-enter");
+                               }
+
+
+                            } while (choice > 0);
+
+                            System.out.println("Enter the vendor phone number: ");
+                            phoneNumber = sc.next();
+                            if (phoneNumber.length() != 11)
+                            {
+                               do
+                               {
+                                  System.out.println("phone number is not 11 digits - please re-enter: ");
+                                  phoneNumber = sc.next();
+                               } while (phoneNumber.length() != 11);
+                            }//if
+                            do
+                            {
+                               System.out.println("Enter the price of the property: £");
+                               price = sc.next();
+                               for (int l = 0; l < n; l++)
+                               {
+                                  if (!Character.isDigit(phoneNumber.charAt(i)))
+                                  {
+                                     System.out.println("Invalid price - please re-enter: ");
+
+                                  }
+
+                               }
+                            } while (!Character.isDigit(phoneNumber.charAt(i)));
+
+                         }
+                      }
+                      Property propertyNew = new Property(street1, town1, postCode, type, vendorName, phoneNumber, price );
+
+                   case '4':
+
+                   case '5':
+                      passwordValidation();
+                      System.out.println("Enter property address: ");
+                      String street = sc.next();
+
+                      for (int i = 0; i < MyArray.getNoOfProperties(); i++)
+                      {
+
+                         MyArray.getStreet(i);
+                         if (street.equals(MyArray.getStreet(i)))
+                         {
+
+                            MyArray.getCurrent(i).displayProperty();
+                            System.out.println("do you want to delete this property (y/n): ");
+                            char answer = sc.next().charAt(0);
+                            do
+                            {
+                               if ((answer == 'y') || (answer == 'Y'))
+                               {
+                                  MyArray.removeProperty(i);
+                                  break;
+                               }
+                               break;
+
+
+                            } while (true);
+                         }
+                      }
                    default:
-                     System.out.println("Invalid choice - please re-enter");
-               }
+                      System.out.println("Invalid choice - please re-enter");
+                }
 
 
 
 
 
-         } while (option != 'o' || option != '1' || option != '2'|| option != '3'|| option != '4'|| option != '5');
+         } while (true);
       }
 }
 
